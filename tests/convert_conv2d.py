6148ce4ad54066efc5c608795d71a5c3d73ede65
Grigory Malivenko <nerox8664@gmail.com>
2018-10-30 20:41:06 +0300

    Updated TDD test for Convolution.

--- tests/convert_conv2d.py
+++ tests/convert_conv2d.py
@@ -20,6 +20,16 @@ class Conv2dTest(mx.gluon.nn.HybridSequential):
         x = self.conv1(x)
         return x
 
+def check_error(gluon_output, pytorch_output, epsilon=1e-5):
+    pytorch_output = pytorch_output.data.numpy()
+    gluon_output = gluon_output.asnumpy()
+
+    error = np.max(pytorch_output - gluon_output)
+    print('Error:', error)
+
+    assert error < epsilon
+    return error
+
 
 if __name__ == '__main__':
     print('Test convolution:')
@@ -32,3 +42,9 @@ if __name__ == '__main__':
     net.collect_params().initialize()
 
     pytorch_model = gluon2pytorch(net, dst_dir='/tmp/gluon2pytorch', pytorch_module_name='Conv2dTest')
+
+    input_np = np.random.uniform(0, 1, (1, 3, 224, 224))
+
+    gluon_output = net(mx.nd.array(input_np))
+    pytorch_output = pytorch_model(torch.FloatTensor(input_np))
+    check_error(gluon_output, pytorch_output)
