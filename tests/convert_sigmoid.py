c01857af4d6d11dca4936440846778d8f94a9600
Grigory Malivenko <nerox8664@gmail.com>
2018-10-30 21:24:30 +0300

    Add sigmoid test.

---
+++ tests/convert_sigmoid.py
@@ -0,0 +1,49 @@
+import argparse
+import torch
+import mxnet as mx
+import numpy as np
+import sys
+
+sys.path.append('../')
+from gluon2pytorch import gluon2pytorch
+
+
+class SigmoidTest(mx.gluon.nn.HybridSequential):
+    def __init__(self):
+        super(SigmoidTest, self).__init__()
+        from mxnet.gluon import nn
+        with self.name_scope():
+            self.conv1 = nn.Conv2D(3, 32)
+            self.sig = nn.Activation('sigmoid')
+
+    def hybrid_forward(self, F, x):
+        x =  F.sigmoid(self.sig(self.conv1(x)))
+        return x
+
+def check_error(gluon_output, pytorch_output, epsilon=1e-5):
+    pytorch_output = pytorch_output.data.numpy()
+    gluon_output = gluon_output.asnumpy()
+
+    error = np.max(pytorch_output - gluon_output)
+    print('Error:', error)
+
+    assert error < epsilon
+    return error
+
+
+if __name__ == '__main__':
+    print('Test sig:')
+
+    net = SigmoidTest()
+    
+    # Make sure it's hybrid and initialized
+    net.hybridize()
+    net.collect_params().initialize()
+
+    pytorch_model = gluon2pytorch(net, dst_dir=None, pytorch_module_name='SigmoidTest')
+
+    input_np = np.random.uniform(-1, 1, (1, 3, 224, 224))
+
+    gluon_output = net(mx.nd.array(input_np))
+    pytorch_output = pytorch_model(torch.FloatTensor(input_np))
+    check_error(gluon_output, pytorch_output)
