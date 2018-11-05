28c82634fe08f80ead4a831e7f157439d40c4282
Grigory Malivenko <nerox8664@gmail.com>
2018-11-05 12:25:27 +0300

    Implement deconvolution.

--- gluon2pytorch/layers.py
+++ gluon2pytorch/layers.py
@@ -54,6 +54,54 @@ def convert_conv(i, op, gluon_nodes, gluon_dict, pytorch_dict):
     return init_str, call_str
 
 
+def convert_deconvolution(i, op, gluon_nodes, gluon_dict, pytorch_dict):
+    """
+    Convert Conv2dTranspose layer.
+    """
+
+    assert(op['attrs']['layout'] == 'NCHW')
+
+    init_tmp = ' ' * 8 +\
+        'self.x{i} = nn.ConvTranspose2d(' +\
+        '{in_channels}, {out_channels}, kernel_size={kernel_size}, stride={strides}, ' +\
+        'bias={use_bias}, groups={num_group}, padding={padding})'
+    call_tmp = ' ' * 8 +\
+        'x{i} = self.x{i}(x{inp})'
+
+    weights = gluon_dict[op['name'] + '_weight'].data().asnumpy()
+
+    pytorch_dict['x{i}.weight'.format(i=i)] = torch.FloatTensor(weights)
+
+    bias = None
+    use_bias = not bool(op['attrs']['no_bias'])
+    if use_bias:
+        bias = gluon_dict[op['name'] + '_bias'].data.asnumpy()
+        pytorch_dict['x{i}.bias'.format(i=i)] = torch.FloatTensor(bias)
+
+    if len(op['inputs']) == 0:
+        input_name = ''
+    else:
+        input_name = op['inputs'][0]
+
+    init_str = init_tmp.format(**{
+        'i': i,
+        'in_channels': weights.shape[0],
+        'out_channels': int(op['attrs']['num_filter']),
+        'use_bias': use_bias,
+        'kernel_size': op['attrs']['kernel'],
+        'strides': op['attrs']['stride'],
+        'padding': op['attrs']['pad'],
+        'num_group': op['attrs']['num_group']
+    })
+
+    call_str = call_tmp.format(**{
+        'i': i,
+        'inp': input_name
+    })
+
+    return init_str, call_str
+
+
 def convert_relu(i, op, gluon_nodes, gluon_dict, pytorch_dict):
     call_tmp = ' ' * 8 + 'x{i} = self.x{i}(x{inp})'
     init_tmp = ' ' * 8 + 'self.x{i} = nn.ReLU()'
@@ -521,4 +569,5 @@ CONVERTERS = {
     'broadcast_mul': convert_elemwise_mul,
     'LeakyReLU': convert_leaky_relu,
     'Pad': convert_pad,
+    'Deconvolution': convert_deconvolution,
 }
