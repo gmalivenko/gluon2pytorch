8ea4519c79b68ed3ccd3a14c87921b04e148673e
Grigory Malivenko <nerox8664@gmail.com>
2018-11-22 16:06:24 +0300

    Add test for multiple outputs.

---
+++ tests/convert_multiple_outputs.py
@@ -0,0 +1,51 @@
+import torch
+import mxnet as mx
+import numpy as np
+from gluon2pytorch import gluon2pytorch
+
+
+class Conv2dTest(mx.gluon.nn.HybridSequential):
+    def __init__(self):
+        super(Conv2dTest, self).__init__()
+        from mxnet.gluon import nn
+        with self.name_scope():
+            self.conv1 = nn.Conv2D(32, 3, 1)
+
+    def hybrid_forward(self, F, x1, x2):
+        r1 = self.conv1(x1) + self.conv1(x2)
+        r2 = r1 + self.conv1(x1)
+        return r1, r2
+
+
+def check_error(gluon_output, pytorch_output, epsilon=1e-5):
+    if not isinstance(pytorch_output, tuple):
+        pytorch_output = [pytorch_output]
+        gluon_output = [gluon_output]
+
+    for p, g in zip(pytorch_output, gluon_output):
+        pytorch_output = p.data.numpy()
+        gluon_output = g.asnumpy()
+
+        error = np.max(pytorch_output - gluon_output)
+        print('Error:', error)
+
+        assert error < epsilon
+        return error
+
+
+if __name__ == '__main__':
+    print('Test convolution:')
+
+    net = Conv2dTest()
+
+    # Make sure it's hybrid and initialized
+    net.hybridize()
+    net.collect_params().initialize()
+
+    pytorch_model = gluon2pytorch(net, [(1, 3, 224, 224), (1, 3, 224, 224)], dst_dir='../tmp', pytorch_module_name='Conv2dTest')
+
+    input_np = np.random.uniform(0, 1, (1, 3, 224, 224))
+
+    gluon_output = net(mx.nd.array(input_np), mx.nd.array(input_np))
+    pytorch_output = pytorch_model(torch.FloatTensor(input_np), torch.FloatTensor(input_np))
+    check_error(gluon_output, pytorch_output)
